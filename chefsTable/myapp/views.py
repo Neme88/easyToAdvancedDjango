307a8b491d8aa0a71520c2c30063cdfd77dbdab7
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-01-21 11:57:51 +0200

    fixed bugs in this two files I am commiting

--- chefsTable/myapp/views.py
+++ chefsTable/myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, World. This is the index view of Demoapp.")
+    return HttpResponse("Hello, World. This is the index view of my first app.")
 
 
 # Create your views here.
